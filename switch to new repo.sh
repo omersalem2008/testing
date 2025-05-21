07c4aa875fc42c4fad782101f23312a937199a2a
omersalem2008 <102019620+omersalem2008@users.noreply.github.com>
2025-05-21 09:21:33 +0300

    Update switch to new repo.sh

--- switch to new repo.sh
+++ switch to new repo.sh
@@ -5,5 +5,5 @@ echo "Clearing cached GitHub credentials..."
 printf "protocol=https\nhost=github.com\n" | git credential-manager erase
 git config --global --unset credential.helper
 git config --system --unset credential.helper
-git remote set-url origin https://<your-correct-username>@github.com/<your-repo-owner>/<repo-name>.git
+git remote set-url origin https://omersalem2008@github.com/omersalem2008/testing.git
 echo "Done. Now run 'git push' and log in with your GitHub PAT."
